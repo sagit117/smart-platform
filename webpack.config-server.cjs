18f3eddfc68db03a282e108b2bbadbf3226b712a
sagit117 <sagit117@gmail.com>
2021-02-25 17:52:28 +0300

    пробы вебпака для ноды

--- webpack.config-server.cjs
+++ webpack.config-server.cjs
@@ -1,5 +1,5 @@
 const path = require('path')
-const HandlebarsPlugin = require("handlebars-webpack-plugin");
+// const HandlebarsPlugin = require("handlebars-webpack-plugin");
 
 const {
     NODE_ENV = 'production',
@@ -10,13 +10,31 @@ module.exports = {
     mode: NODE_ENV,
     target: 'node',
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, '/build-server'),
         filename: 'index.js'
     },
     resolve: {
-        extensions: ['.ts', '.js', 'hbs'],
+        extensions: ['.ts', '.js', '.hbs'],
     },
-    plugins: [
-        new HandlebarsPlugin()
-    ]
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.handlebars$/,
+                loader:  "handlebars-loader",
+                // query: {
+                //     partialDirs: [
+                //         path.join(__dirname, 'views', 'partials')
+                //     ],
+                //     // helperDirs: [
+                //     //     path.join(__dirname, 'views', 'helpers')
+                //     // ]
+                // }
+            }
+        ]
+    },
 }
