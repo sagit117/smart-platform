f453c21e6962523cce274871f8289ecae3569416
Павел Аксенов <sagit117@gmail.com>
2021-03-08 23:34:32 +0300

    translate not found page

--- public/js/components/app/NotFoundComponent.vue
+++ public/js/components/app/NotFoundComponent.vue
@@ -1,3 +1,20 @@
 <template>
-  Not found
+  <h3>{{ L.translate('Ресурс не найден') }}</h3>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import Lang from '../../../../src/dictionary/language'
+const L = new Lang(localStorage.getItem('language') || 'rus')
+
+export default defineComponent({
+  name: 'not-found',
+
+  setup() {
+    return {
+      L
+    }
+  }
+})
+</script>
